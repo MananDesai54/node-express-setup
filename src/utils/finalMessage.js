f84337267e861154099f7b8579efd7a363a674fb
MananDesai54 <manan5401desai@gmail.com>
2021-06-23 12:29:20 +0530

    with git

--- src/utils/finalMessage.js
+++ src/utils/finalMessage.js
@@ -1,4 +1,5 @@
 const color = require("cli-color");
+const { executeCommand } = require("./executeCommand");
 
 module.exports = {
   finalMessage: (language, folderName) => {
@@ -13,15 +14,18 @@ module.exports = {
     let message;
     if (language === "JS") {
       message = `
+        Initialized Git and
         Run: node src/index.js
       `;
     } else {
       message = `
+        Initialized Git
         Run: tsc -w
         Run: node dist/index.js
       `;
     }
     console.log();
+    executeCommand("git init", folderName ? folderName : ".");
     console.log(color.blue(message));
   },
 };
